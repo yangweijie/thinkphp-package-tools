7d7703f1c52435f83fb5e6c40ace00ed96a0edfe
jay <917647288@qq.com>
2025-05-05 09:45:58 +0800

    添加vendor:pub 命令

--- src/concerns/package/HasVendorPublishCommand.php
+++ src/concerns/package/HasVendorPublishCommand.php
@@ -2,7 +2,18 @@
 
 namespace yangweijie\thinkphpPackageTools\concerns\package;
 
+use yangweijie\thinkphpPackageTools\command\VendorPublishCommand;
+
 trait HasVendorPublishCommand
 {
+    public function VendorPublishCommand($callable): static
+    {
+        $installCommand = new VendorPublishCommand($this);
 
+        $callable($installCommand);
+
+        $this->consoleCommands[] = $installCommand;
+
+        return $this;
+    }
 }
